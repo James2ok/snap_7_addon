efc7bd2f029cc98283c96f9cc928129f50be66ac
James2ok <105300406+James2ok@users.noreply.github.com>
2025-12-17 16:21:20 +0100

    Update main.py

--- snap7-addon/main.py
+++ snap7-addon/main.py
@@ -1,8 +1,8 @@
 import snap7
-from snap7.util import *
+from snap7.util import get_int
 from snap7.snap7types import *
 
-PLC_IP = "192.168.1.1"  # Cambia con l'IP del tuo PLC
+PLC_IP = "192.168.1.100"  # Cambia con l'IP del tuo PLC
 RACK = 0
 SLOT = 1
 
@@ -11,7 +11,6 @@ client.connect(PLC_IP, RACK, SLOT)
 
 if client.get_connected():
     print("Connesso al PLC!")
-    # Esempio: leggere DB1, 4 byte dall'offset 0
     data = client.db_read(1, 0, 4)
     value = get_int(data, 0)
     print("Valore letto:", value)
